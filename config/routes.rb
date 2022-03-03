96b15ae9c78ff2f80581332527c2b2256610713c
alexiuadrian <adi.alexiu@gmail.com>
2022-03-03 19:41:51 +0200

    added group chats functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
+  get "rooms/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  Rails.application.routes.draw do
+    get "rooms/index"
+    get "/signin", to: "sessions#new"
+    post "/signin", to: "sessions#create"
+    delete "/signout", to: "sessions#destroy"
+  end
+
+  resources :rooms do
+    resources :messages
+  end
+
+  resources :rooms
+  resources :users
+  root "rooms#index"
 end
